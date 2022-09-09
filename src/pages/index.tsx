2b22b0ccc31fe2ef6ee577cac349b92f5c183935
Rafael Guimaraes <rcarvalhobsb@gmail.com>
2022-09-09 15:28:16 -0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>E-Commerce</title>
+      </Head>
+
+      <main>
+        <h1>Hello World!</h1>
+      </main>
+    </div>
+  );
+}

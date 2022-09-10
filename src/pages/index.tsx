1f18f0a7aea376fa29eb02eef7eafe8fc5008f8a
Rafael Guimaraes <rcarvalhobsb@gmail.com>
2022-09-10 13:00:35 -0300

    feat: add banner to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
+import Image from "next/future/image";
 import Head from "next/head";
+import bannerImage from "../assets/banner-image.png";
+
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
@@ -6,7 +10,30 @@ export default function Home() {
       <Head>
         <title>Home | Gamer Shop </title>
       </Head>
-      <h1>Hello World!</h1>
+
+      <main className="container">
+        <div className={styles.banner}>
+          <div className={styles.bannerInfo}>
+            <span>HyperX Cloud Stinger</span>
+            <span>Headphone</span>
+            <button>Adicione ao carrinho</button>
+          </div>
+
+          <div className={styles.bannerImageContainer}>
+            <Image src={bannerImage} alt="" className="" />
+          </div>
+
+          <div className={styles.descriptionContainer}>
+            <h2>Descrição</h2>
+            <p>
+              Completa e versátil, a linha de headsets Cloud foi projetada para
+              as necessidades de qualquer nível de jogador. Independente do seu
+              sistema, estilo de jogo e características este headset irá te
+              surpreender.
+            </p>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }

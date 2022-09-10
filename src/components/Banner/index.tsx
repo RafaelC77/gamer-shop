04c74425c1b12ab27c2ae33fdae98bfa3d799e86
Rafael Guimaraes <rcarvalhobsb@gmail.com>
2022-09-10 18:48:44 -0300

    feat: set banner image size

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -4,28 +4,30 @@ import styles from "./Banner.module.scss";
 
 export function Banner() {
   return (
-    <div className={styles.banner}>
-      <div className={styles.bannerInfo}>
-        <span>HyperX Cloud Stinger</span>
-        <span>
-          44% OFF <b>R$ 381,41</b> R$ 179,99
-        </span>
-        <span>Headphone</span>
-        <button>Adicione ao carrinho</button>
-      </div>
+    <div className="container">
+      <div className={styles.bannerContent}>
+        <div className={styles.bannerInfo}>
+          <span>HyperX Cloud Stinger</span>
+          <span>
+            44% OFF <b>R$ 381,41</b> R$ 179,99
+          </span>
+          <span>Headphone</span>
+          <button>Adicione ao carrinho</button>
+        </div>
 
-      <div className={styles.bannerImageContainer}>
-        <Image src={bannerImage} alt="" className="" />
-      </div>
+        <div className={styles.bannerImageContainer}>
+          <Image src={bannerImage} alt="" className={styles.bannerImage} />
+        </div>
 
-      <div className={styles.descriptionContainer}>
-        <h2>Descrição</h2>
-        <p>
-          Completa e versátil, a linha de headsets Cloud foi projetada para as
-          necessidades de qualquer nível de jogador. Independente do seu
-          sistema, estilo de jogo e características este headset irá te
-          surpreender.
-        </p>
+        <div className={styles.descriptionContainer}>
+          <h2>Descrição</h2>
+          <p>
+            Completa e versátil, a linha de headsets Cloud foi projetada para as
+            necessidades de qualquer nível de jogador. Independente do seu
+            sistema, estilo de jogo e características este headset irá te
+            surpreender.
+          </p>
+        </div>
       </div>
     </div>
   );

8351a78688b0e936b50e71549c73e526fd704c6c
Rafael Guimaraes <rcarvalhobsb@gmail.com>
2022-11-03 11:03:47 -0300

    test: add tests to completeOrderButton component

--- src/tests/components/CompleteOrderButton.spec.tsx
+++ src/tests/components/CompleteOrderButton.spec.tsx
@@ -1,9 +1,39 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import "whatwg-fetch";
+
+import { getStripeJs } from "../../services/stripe-js";
+import { CartContext } from "../../contexts/CartContext";
 import { CompleteOrderButton } from "../../components/CompleteOrderButton";
+import { ToastContainer } from "react-toastify";
+
+jest.mock("../../services/stripe-js");
+
+const server = setupServer(
+  rest.post("/api/complete-order", (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({ id: "responseID", status: "open" }));
+  })
+);
+
+beforeAll(() => server.listen());
+afterAll(() => server.close());
+afterEach(() => server.resetHandlers());
 
 describe("CompleteOrderButton Component", () => {
   it("Should render correctly", () => {
-    render(<CompleteOrderButton />);
+    const cartContextValues = {
+      shoppingCart: ["item"],
+      setCartItem: jest.fn(),
+      updateCart: jest.fn(),
+      resetCart: jest.fn(),
+    };
+
+    render(
+      <CartContext.Provider value={cartContextValues}>
+        <CompleteOrderButton />
+      </CartContext.Provider>
+    );
 
     screen.logTestingPlaygroundURL();
 
@@ -12,5 +42,90 @@ describe("CompleteOrderButton Component", () => {
         name: /finalizar pedido/i,
       })
     ).toBeInTheDocument();
+  });
+
+  it("Should be disabled when cart is empty", () => {
+    const cartContextValues = {
+      shoppingCart: [],
+      setCartItem: jest.fn(),
+      updateCart: jest.fn(),
+      resetCart: jest.fn(),
+    };
+
+    render(
+      <CartContext.Provider value={cartContextValues}>
+        <CompleteOrderButton />
+      </CartContext.Provider>
+    );
+
+    expect(
+      screen.getByRole("button", {
+        name: /finalizar pedido/i,
+      })
+    ).toBeDisabled();
+  });
+
+  it("Should redirect to stripe checkout", async () => {
+    const cartContextValues = {
+      shoppingCart: ["item"],
+      setCartItem: jest.fn(),
+      updateCart: jest.fn(),
+      resetCart: jest.fn(),
+    };
+
+    const mockedStripe = jest.mocked(getStripeJs);
+    const redirectMock = jest.fn();
+
+    mockedStripe.mockResolvedValueOnce({
+      redirectToCheckout: redirectMock,
+    } as any);
+
+    render(
+      <CartContext.Provider value={cartContextValues}>
+        <CompleteOrderButton />
+      </CartContext.Provider>
+    );
+
+    const completeOrderButton = screen.getByRole("button", {
+      name: /finalizar pedido/i,
+    });
+
+    fireEvent.click(completeOrderButton);
+
+    await waitFor(() => expect(redirectMock).toBeCalled());
+  });
+
+  it("Should show a toast if the api call fails", async () => {
+    const cartContextValues = {
+      shoppingCart: ["item"],
+      setCartItem: jest.fn(),
+      updateCart: jest.fn(),
+      resetCart: jest.fn(),
+    };
+
+    server.use(
+      rest.post("/api/complete-order", (req, res, ctx) => {
+        return res(ctx.status(500), ctx.json("error"));
+      })
+    );
+
+    render(
+      <CartContext.Provider value={cartContextValues}>
+        <CompleteOrderButton />
+        <ToastContainer />
+      </CartContext.Provider>
+    );
+
+    const completeOrderButton = screen.getByRole("button", {
+      name: /finalizar pedido/i,
+    });
+
+    fireEvent.click(completeOrderButton);
+
+    await waitFor(() =>
+      expect(
+        screen.getByText("Houve um erro ao realizar o pedido!")
+      ).toBeInTheDocument()
+    );
   });
 });
